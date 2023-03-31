93c105651d4f2266779f1f072877de810679cd90
fish <fishpigbird.@gmail.com>
2023-03-31 23:08:39 +0800

    addd uttros

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   sortByDate: false,
   showAbout: true,
   showArchive: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar 自动折叠的导航栏-》顶栏收缩
+  autoCollapsedNavBar: true, // The automatically collapsed navigation bar 自动折叠的导航栏-》顶栏收缩
   ogImageGenerateURL: 'https://raw.githubusercontent.com/fishpigbird/nobelium/main/public/ogimage1.png.png', // The link to generate OG image, don't end with a slash    //https://og-playground.vercel.app/
   socialLink: 'https://fpb.icu',
   seo: {
@@ -38,7 +38,7 @@ const BLOG = {
   },
   comment: {
     // support provider: gitalk, utterances, cusdis
-    provider: '', // leave it empty if you don't need any comment plugin
+    provider: 'utterances', // leave it empty if you don't need any comment plugin
     gitalkConfig: {
       repo: '', // The repository of store comments
       owner: '',
